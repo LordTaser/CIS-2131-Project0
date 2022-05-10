44ea1fc42a3a06c2489209aad1527ee37479e755
LordTaser <79674884+LordTaser@users.noreply.github.com>
2022-05-10 08:59:33 -0400

    Upload attempt 2

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ print("My name is Kyle. I'm taking this class to expand the number of Programmin
 print("I have taken Java 1 and 2, as well as C++ and SQL.")
 print("I'm an avid video game player. I have my own game projects in mind. Just looking for the right people to help.")
 print("School and fun time with the kids on the weekends.")
-
