f8a9a48d495ab265ed53758880d49d76f4a2fe81
LordTaser <79674884+LordTaser@users.noreply.github.com>
2022-05-10 08:54:15 -0400

    K.Givinsky - Project 0
    Project 0 for Kyle Givinsky

---
+++ main.py
@@ -0,0 +1,4 @@
+print("My name is Kyle. I'm taking this class to expand the number of Programming Languages that I am familiar with.")
+print("I have taken Java 1 and 2, as well as C++ and SQL.")
+print("I'm an avid video game player. I have my own game projects in mind. Just looking for the right people to help.")
+print("School and fun time with the kids on the weekends.")
