4419fcda985d537a3a4aa0be0de87b03f752f280
LordTaser <79674884+LordTaser@users.noreply.github.com>
2022-05-10 17:37:22 -0400

    Additional Information Supplied

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ print("My name is Kyle. I'm taking this class to expand the number of Programmin
 print("I have taken Java 1 and 2, as well as C++ and SQL.")
 print("I'm an avid video game player. I have my own game projects in mind. Just looking for the right people to help.")
 print("School and fun time with the kids on the weekends.")
+print("I take to programming like a fish takes to water. I typically can understand a great deal without asking "
+      "questions. But will not hesitate to ask if I am stuck or don't fully understand.")
